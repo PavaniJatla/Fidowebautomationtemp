59e431ee6ca6163ed7e31fbb87265c8d565e3d1c
mirzakamran <62909581+mirzakamran@users.noreply.github.com>
2020-11-16 09:52:11 -0500

    Dec 2nd QA5 url updated TC75 updated

---
+++ src/ca/fido/test/tests/selfserve/desktop/FidoSS_Regression_TC075_AO_ValidateMakePaymentCTAinCLMNotification_BC.java
@@ -0,0 +1,68 @@
+package ca.fido.test.tests.selfserve.desktop;
+
+import ca.fido.test.base.BaseTestClass;
+import ca.fido.test.helpers.FidoEnums;
+import ca.fido.testdatamanagement.TestDataHandler;
+import org.apache.http.client.ClientProtocolException;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * This test will check the change method of payment functionality
+ * works on French and English both
+ * @author Mirza.Kamran
+ *
+ */
+public class FidoSS_Regression_TC075_AO_ValidateMakePaymentCTAinCLMNotification_BC extends BaseTestClass{
+
+	@BeforeMethod(alwaysRun = true)   @Parameters({ "strBrowser", "strLanguage"})
+	public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage, ITestContext testContext,Method method) throws ClientProtocolException, IOException {
+		// xmlTestParameters = new HashMap<String, String>(testContext.getCurrentXmlTest().getAllParameters());
+		startSession(System.getProperty("QaUrl"), strBrowser,
+				strLanguage, FidoEnums.GroupName.selfserve,method);			
+	}
+	
+
+	
+	@AfterMethod(alwaysRun = true)
+	public void afterTest() throws InterruptedException {
+		closeSession();
+	}
+	
+	
+
+	@Test(groups = {"AccountOverviewSS"})
+	public void postPaidChangeMOP() {
+		getDriver().get(System.getProperty("QaUrl")+"/self-serve/overview");
+		//fido_home_page.clkLogin();
+		fido_login_page.switchToSignInFrame();
+		fido_login_page.setUsernameInFrame(TestDataHandler.tc121315.getUsername());
+		fido_login_page.setPasswordInFrame(TestDataHandler.tc121315.getPassword());
+		reporter.reportLogWithScreenshot("Login Credential is entered.");
+		fido_login_page.clkLoginInFrame();	
+		reporter.hardAssert(!fido_login_page.verifyIfErrorMsgIsDisplayedInFrame(), 
+				"Login proceed without error.", 
+				"Login failed with error.");
+		fido_login_page.switchOutOfSignInFrame();
+		reporter.hardAssert(fido_account_overview_page.verifySuccessfulLoginNew(), 
+				"Login succeed.", 
+				"Failed to login.");
+		reporter.reportLogWithScreenshot("Account overview page");
+				
+		reporter.hardAssert(fido_account_overview_page.verifyIfAtleastOneUserHasAutoPaymentSet(),
+				"The user has atleast 1 active account which has already automatic payments option",
+				"The user should have atleast 1 active account which has already automatic payments option");
+		
+		fido_account_overview_page.clkPenIconForChangePaymentMethod();
+		reporter.hardAssert(fido_payment_options_page.verifyPaymentMethodModalDisplayed(),
+				"Change payment method modal displayed.",
+				"Change payment method modal didn't display as expected.");
+							
+	}
+	
+	
+
+}
