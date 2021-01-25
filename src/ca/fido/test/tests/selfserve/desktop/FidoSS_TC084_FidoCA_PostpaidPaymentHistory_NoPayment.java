574ff9df55ac74dad10d957bf6a23a2305341c26
mirzakamran <62909581+mirzakamran@users.noreply.github.com>
2021-01-25 10:43:16 -0500

    test data update

---
+++ src/ca/fido/test/tests/selfserve/desktop/FidoSS_TC084_FidoCA_PostpaidPaymentHistory_NoPayment.java
@@ -0,0 +1,69 @@
+package ca.fido.test.tests.selfserve.desktop;
+
+import ca.fido.pages.FidoAccountOverviewPage;
+import ca.fido.test.base.BaseTestClass;
+import ca.fido.test.helpers.FidoEnums;
+import ca.fido.testdatamanagement.TestDataHandler;
+import org.apache.http.client.ClientProtocolException;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * 
+ * @author Mirza.Kamran
+ *
+ */
+public class FidoSS_TC084_FidoCA_PostpaidPaymentHistory_NoPayment extends BaseTestClass{
+
+	@BeforeMethod(alwaysRun = true)   @Parameters({ "strBrowser", "strLanguage"})
+	public void beforeTest(@Optional("chrome") String strBrowser, @Optional("en") String strLanguage, ITestContext testContext,Method method) throws ClientProtocolException, IOException {
+		// xmlTestParameters = new HashMap<String, String>(testContext.getCurrentXmlTest().getAllParameters());
+		startSession(System.getProperty("QaUrl"), strBrowser,
+				strLanguage, FidoEnums.GroupName.selfserve,method);			
+	}
+	
+
+	
+	@AfterMethod(alwaysRun = true)
+	public void afterTest() throws InterruptedException {
+		closeSession();
+	}
+	
+	@Test(groups = {"RegressionSS","BillingAndPaymentsSS"})
+	public void postPaidPaymentHistoryDataValidations() throws InterruptedException {
+		
+		getFidohomepage().clkLogin();
+		getFidologinpage().switchToSignInFrame();
+		getFidologinpage().setUsernameInFrame("Auto121SingleJan27Set3@yahoo.com");
+		getFidologinpage().setPasswordInFrame("DigiAuto@123");
+		getReporter().reportLogWithScreenshot("Login Credential is entered.");
+		getFidologinpage().clkLoginInFrame();	
+		getReporter().hardAssert(!getFidologinpage().verifyIfErrorMsgIsDisplayedInFrame(), 
+				"Login proceed without error.", 
+				"Login failed with error.");
+		getFidologinpage().switchOutOfSignInFrame();
+		getReporter().hardAssert(getFidoaccountoverviewpage().verifySuccessfulLogin(), 
+				"Login succeed.", 
+				"Failed to login.");
+		String strBAN = "940642549";//TestDataHandler.tc121315.getaccountDetails().getBan();
+		getFidoaccountoverviewpage().clkViewBillNew(strBAN);
+		getReporter().reportLogWithScreenshot("View bill page is open");
+		getFidoaccountoverviewpage().clkMenuBillingAndPayments();
+		getReporter().reportLogWithScreenshot("Menu Billings and payments selected");
+		getFidoaccountoverviewpage().selectBillingAndpaymentsSubMenus(FidoAccountOverviewPage.BillingAndPaymentsSubMenuOptions.PaymentHistory);
+		//getReporter().softAssert(getFidopaymenthistorypage().getTotalRowCount()!=0,"","The row count is blank it seem");
+						
+			getReporter().hardAssert(getFidopaymenthistorypage().verifyNoPaymentRecordMessage(),
+					"No payment history records present yet for this account, please make some transactions",
+					"The label No payment history records is not shown and the payment history table is also not displayed, please investigate");
+			
+			getReporter().reportLogWithScreenshot("No Payment history record view");
+	
+								
+		
+	}
+
+}
