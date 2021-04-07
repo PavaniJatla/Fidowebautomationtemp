6c62dc1e8620cd0127341af03572f13c75926eb5
RCI\Rohit.Kumar5 <rohit.kumar5@rci.rogers.com>
2021-04-07 09:37:04 -0400

    xpath updated

--- src/ca/fido/pages/FidoWirelessDashboardPostpaidPage.java
+++ src/ca/fido/pages/FidoWirelessDashboardPostpaidPage.java
@@ -136,7 +136,7 @@ public class FidoWirelessDashboardPostpaidPage extends BasePageClass {
 	
 	//My device Section
 	@FindAll({
-	@FindBy(xpath = "//*[@translate='wirelessDashboard.deviceSection.deviceTitle' or text()='my device' or text()='mon appareil' or text()='Fido Payment Program']/parent::div//following-sibling::div[contains(@class,'device-wrapper')]"),
+	@FindBy(xpath = "//*[@translate='wirelessDashboard.deviceSection.deviceTitle' or text()='my device' or text()='Programme de paiement Fido' or text()='Fido Payment Program']/parent::div//following-sibling::div[contains(@class,'device-wrapper')]"),
 	@FindBy (xpath = "//div[@translate='wirelessDashboard.deviceSection.deviceTitle']/parent::div/following-sibling::div[contains(@class,'device-wrapper')]")})
 	WebElement divMyDevice;
 
