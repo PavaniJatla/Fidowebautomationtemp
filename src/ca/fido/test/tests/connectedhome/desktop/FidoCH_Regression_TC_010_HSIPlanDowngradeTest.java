0337f1c8ed85cfbe7805a85e2ed423a2a17176b0
ChinnaraoVattam <chinnarao.vattam@rci.rogers.com>
2021-04-13 15:04:42 -0400

    Updated data commit for sanity

--- src/ca/fido/test/tests/connectedhome/desktop/FidoCH_Regression_TC_010_HSIPlanDowngradeTest.java
+++ src/ca/fido/test/tests/connectedhome/desktop/FidoCH_Regression_TC_010_HSIPlanDowngradeTest.java
@@ -28,7 +28,7 @@ import java.lang.reflect.Method;
 
 public class FidoCH_Regression_TC_010_HSIPlanDowngradeTest extends BaseTestClass {
 
-	@Test(groups = {"SanityCH","RegressionCH","FidoUpgradePlanCH"})
+	@Test(groups = {"SanityCH","RegressionCH","FidoHSIDashboardCH"})
 	public void checkFidoHSIPlanDowngrade() {
 		getReporter().reportLogWithScreenshot("Launched the Home Page");
 		getFidohomepage().clkLogin();
