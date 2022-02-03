9e2d808c4b4bcf02020ef1bf78b2227d50a85de2
Ning.Xue <>
2022-02-03 13:17:02 -0500

    Remove unused dependency and imports.

--- src/ca/fido/test/listeners/Retry.java
+++ src/ca/fido/test/listeners/Retry.java
@@ -1,12 +1,5 @@
 package ca.fido.test.listeners;
 
-
-import ca.fido.test.base.BaseTestClass;
-import com.relevantcodes.extentreports.LogStatus;
-import extentreport.ExtentTestManager;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
